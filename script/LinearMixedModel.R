b114bb38db88cec42fc8e28759347778e9473162
samanthasolisdeovando <slsolisdeovando@gmail.com>
2026-03-02 15:23:45 -0800

    linear mixed model

---
+++ script/LinearMixedModel.R
@@ -0,0 +1,12 @@
+############ source: Our Coding Club ###########
+###### edited by Samantha Solis de Ovando ######
+
+library(readr)
+master_data_modelling <- read_csv("MasterDataSheet_UGthesis_Feb232026.csv")
+
+head(master_data_modelling)
+
+hist(master_data_modelling$CO2flux)
+
+basic.lm <- lm(CH4flux ~ MeanSoilMoisture, data = master_data_modelling)
+summary(basic.lm)
