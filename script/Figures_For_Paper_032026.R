2c0d0d3b77d14fccef30402c4f0fd601c6531ab7
samanthasolisdeovando <slsolisdeovando@gmail.com>
2026-03-04 02:13:53 -0800

    fixed non-numerical data & added more figures (scatter only, plant productivity)

--- script/Figures_For_Paper_032026.R
+++ script/Figures_For_Paper_032026.R
@@ -21,6 +21,7 @@ shifted_data <- original_data %>%
 
 ### IMPORTANT: USE THIS DATA WHEN PLOTTING BY TIME
 plotting_with_time <- read.csv("shifted_dataset.csv")
+plotting_with_time$canopyheight <- as.numeric(as.character(plotting_with_time$canopyheight))
 
 
 ############## PLANT PRODUCTIVITY ##############
@@ -88,8 +89,12 @@ ggplot(plotting_with_time, aes(x = measurement.week, y = meanGCC, color = Treatm
 ###################### CANOPY HEIGHT ######################
 
 ### trouble shooting - plots not coming out right:
-str(plotting_with_time$canopyheight)
+#class(plotting_with_time$canopyheight)
+#str(plotting_with_time$canopyheight)
+#plotting_with_time$canopyheight <- as.numeric(as.character(plotting_with_time$canopyheight))
 
+### scatter with linear regression line ###
+#file name: CanopyHeight_Scatter_LinearRegression
 library(ggplot2)
 
 ggplot(plotting_with_time, aes(x = measurement.week, y = canopyheight, color = Treatment)) +
@@ -112,6 +117,91 @@ ggplot(plotting_with_time, aes(x = measurement.week, y = canopyheight, color = T
   labs(
     x = "Week",
     y = "Canopy Height"
+  ) +
+  theme_minimal()
+
+### Canopy height scatter with local regression line ###
+#file name: CanopyHeight_Scatter_LocalRegression
+
+library(ggplot2)
+
+ggplot(plotting_with_time, aes(x = measurement.week, y = canopyheight, color = Treatment)) +
+  geom_point(alpha = 0.6, na.rm = TRUE) +
+  geom_smooth(aes(fill = Treatment),
+              method = "loess",
+              se = TRUE,
+              alpha = 0.2,
+              na.rm = TRUE) +
+  scale_color_manual(values = c(
+    "Control" = "#1E88E5",
+    "Extended" = "#228B22",
+    "Heatwave" = "#F57F17"
+  )) +
+  scale_fill_manual(values = c(
+    "Control" = "#1E88E5",
+    "Extended" = "#228B22",
+    "Heatwave" = "#F57F17"
+  )) +
+  labs(
+    x = "Week",
+    y = "Canopy Height"
+  ) +
+  theme_minimal()
+
+###################### COVER ######################
+### cover scatter with linear regression
+# file name for figure: Cover_Scatter_LinearRegression
+
+library(ggplot2)
+
+ggplot(plotting_with_time, aes(x = measurement.week, y = CanopyExtent, color = Treatment)) +
+  geom_point(alpha = 0.6, na.rm = TRUE) +
+  geom_smooth(aes(fill = Treatment),
+              method = "lm",
+              se = TRUE,
+              alpha = 0.2,
+              na.rm = TRUE) +
+  scale_color_manual(values = c(
+    "Control" = "#1E88E5",
+    "Extended" = "#228B22",
+    "Heatwave" = "#F57F17"
+  )) +
+  scale_fill_manual(values = c(
+    "Control" = "#1E88E5",
+    "Extended" = "#228B22",
+    "Heatwave" = "#F57F17"
+  )) +
+  labs(
+    x = "Week",
+    y = "Cover"
+  ) +
+  theme_minimal()
+
+### cover scatter with local regression ###
+# file name: Cover_Scatter_LocalRegression
+
+library(ggplot2)
+
+ggplot(plotting_with_time, aes(x = measurement.week, y = CanopyExtent, color = Treatment)) +
+  geom_point(alpha = 0.6, na.rm = TRUE) +
+  geom_smooth(aes(fill = Treatment),
+              method = "loess",
+              se = TRUE,
+              alpha = 0.2,
+              na.rm = TRUE) +
+  scale_color_manual(values = c(
+    "Control" = "#1E88E5",
+    "Extended" = "#228B22",
+    "Heatwave" = "#F57F17"
+  )) +
+  scale_fill_manual(values = c(
+    "Control" = "#1E88E5",
+    "Extended" = "#228B22",
+    "Heatwave" = "#F57F17"
+  )) +
+  labs(
+    x = "Week",
+    y = "Cover"
   ) +
   theme_minimal()
 
