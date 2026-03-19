ed26c5bd45cdb5c1161a56fcb49138807dda853b
samanthasolisdeovando <slsolisdeovando@gmail.com>
2026-03-19 11:56:10 -0700

    updated comment (no changes to actual code)

--- script/Figures_For_Paper_032026.R
+++ script/Figures_For_Paper_032026.R
@@ -5,7 +5,7 @@
 # set working directory 
 setwd("~/Documents/UndergradThesis/data")
 
-####### shift extended grp by -2 weeks #######
+####### shift extended grp by -3 weeks #######
 library(dplyr)
 original_data <- read.csv("MasterDataSheet_UGthesis_Feb232026.csv")
 shifted_data <- original_data %>%
