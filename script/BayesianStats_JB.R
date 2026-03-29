320147d552fba6580512f96f380ade970ef3d585
samanthasolisdeovando <slsolisdeovando@gmail.com>
2026-03-29 15:49:08 -0700

    updated GCC priors

--- script/BayesianStats_JB.R
+++ script/BayesianStats_JB.R
@@ -55,6 +55,7 @@ ggboxplot(autumn_cold_data, x = "Treatment", y = "CO2flux")
 #written by Juliana Dioquino
 #edited by Samantha Solis de Ovando
 
+#canopyheight
 ggplot(data = thesis_data, aes(x = Treatment, y = canopyheight, fill = Treatment)) +
   geom_boxplot(alpha = 0.8) +
   facet_grid(~factor(Season, levels=c('Summer', 'Heatwave', 'Autumn warm', 'Autumn cold')))+
@@ -78,7 +79,29 @@ ggplot(data = thesis_data, aes(x = Treatment, y = canopyheight, fill = Treatment
         panel.spacing = unit(1, "lines"),
         panel.border = element_rect(color = "grey", fill = NA, size = 1))
 
-
+#GCC
+ggplot(data = thesis_data, aes(x = Treatment, y = meanGCC, fill = Treatment)) +
+  geom_boxplot(alpha = 0.8) +
+  facet_grid(~factor(Season, levels=c('Summer', 'Heatwave', 'Autumn warm', 'Autumn cold')))+
+  labs(x = "Treatment",
+       y = "Greenness Chromatic Coordinate (GCC)") +
+  geom_jitter(width = 0.12, alpha = 0.4, size = 1) +
+  scale_fill_manual(values = c("Control" = "#1F77B4",
+                               "Heatwave" = "#FF7F0E",
+                               "Extended" = "#2CA02C")) +
+  theme_classic() +
+  theme(legend.position="bottom",
+        legend.title = element_blank(),
+        legend.text = element_text(size = 9),
+        strip.background = element_blank(), #element_rect(colour = "grey", fill = "#EDEDED"),
+        strip.text.x = element_text(size = 9, colour = "black"),
+        axis.text = element_text(size = 9),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.x = element_text(size = 13, margin = margin(t = 12)),
+        axis.title.y = element_text(size = 13, margin = margin(r = 12)),
+        panel.spacing = unit(1, "lines"),
+        panel.border = element_rect(color = "grey", fill = NA, size = 1))
 
 #Kruskal-Test (rstatix)
 res.kruskal <- thesis_data %>% kruskal_test(Cover ~ Season)
@@ -139,28 +162,29 @@ plot(model_co2) # assess model convergence, eith fuzzy caterpillar
 pp_check(model_co2)
 summary(model_co2)
 
-#### GCC bayesian ###
+#### Bayesian GCC ###
+
 library(brms)
 
 ## defining my prior knowlkedge
 # intercept, baseline GCC of the control plot of a given season
-my_prior_gcc <- set_prior("normal(0.325,0.05)",class = "b",coef = "SeasonAutumncold") ### first number 1.5 (first) is mean, 1.5 (second) is stdev
-my_prior_gcc <- c(my_prior_co2,set_prior("normal(0.325,0.06)",class = "b",coef = "SeasonAutumnwarm"))
-my_prior_gcc <- c(my_prior_co2,set_prior("normal(0.325,0.05)",class = "b",coef = "SeasonHeatwave"))
-my_prior_gcc <- c(my_prior_co2,set_prior("normal(0.325,0.05)",class = "b",coef = "SeasonSummer"))
+my_prior_gcc <- set_prior("normal(0.33,0.04)",class = "b",coef = "SeasonAutumncold") ### first number 1.5 (first) is mean, 1.5 (second) is stdev
+my_prior_gcc <- c(my_prior_co2,set_prior("normal(0.33,0.04)",class = "b",coef = "SeasonAutumnwarm"))
+my_prior_gcc <- c(my_prior_co2,set_prior("normal(0.32,0.04)",class = "b",coef = "SeasonHeatwave"))
+my_prior_gcc <- c(my_prior_co2,set_prior("normal(0.315,0.04)",class = "b",coef = "SeasonSummer"))
 # treatment effect of autumn cold
-my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.08)",class = "b",coef = "TreatmentExtended")) #treatment centered around 0 because we dont have any expectsation htat treatment will do anyhting 
-my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.08)",class = "b",coef = "TreatmentHeatwave"))
+my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.04)",class = "b",coef = "TreatmentExtended")) #treatment centered around 0 because we dont have any expectsation htat treatment will do anyhting 
+my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.04)",class = "b",coef = "TreatmentHeatwave"))
 # treatmemt effect within a season (except autumn cold, autumn cold was used as a control!)
-my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.08)",class = "b",coef = "SeasonAutumnwarm:TreatmentHeatwave"))
-my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.08)",class = "b",coef = "SeasonHeatwave:TreatmentHeatwave"))
-my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.08)",class = "b",coef = "SeasonSummer:TreatmentHeatwave"))
+my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.04)",class = "b",coef = "SeasonAutumnwarm:TreatmentHeatwave"))
+my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.04)",class = "b",coef = "SeasonHeatwave:TreatmentHeatwave"))
+my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.04)",class = "b",coef = "SeasonSummer:TreatmentHeatwave"))
 
-my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.08)",class = "b",coef = "SeasonAutumnwarm:TreatmentExtended"))
-my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.08)",class = "b",coef = "SeasonHeatwave:TreatmentExtended"))
-my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.08)",class = "b",coef = "SeasonSummer:TreatmentExtended"))
+my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.04)",class = "b",coef = "SeasonAutumnwarm:TreatmentExtended"))
+my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.04)",class = "b",coef = "SeasonHeatwave:TreatmentExtended"))
+my_prior_gcc <- c(my_prior_co2,set_prior("normal(0,0.04)",class = "b",coef = "SeasonSummer:TreatmentExtended"))
 
-
+#### should i add residuals or random effects priors??
 
 
 model_gcc <- brm(meanGCC | trunc(lb = 0)~ 0+Season*Treatment + (1|PotID), # model formula * = interqcti, no negative values--trunc(lb = 0)
@@ -168,13 +192,14 @@ model_gcc <- brm(meanGCC | trunc(lb = 0)~ 0+Season*Treatment + (1|PotID), # mode
                  family = gaussian(), # the data looks normally distributed
                  iter = 6000, # number of computation, the more the better
                  warmup = 2000, # number of discarded computation 
-                 cores = 3, # this is to speed up the co;putation
+                 cores = 3, # this is to speed up the computation
                  prior = my_prior_co2,
-                 chains = 3,# this is the nu;ber of chain, independant model
+                 chains = 3,# this is the number of chain, independent model
                  init = 0) # makes the computation more stable 
 
 plot(model_gcc) # assess model convergence, eith fuzzy caterpillar
 pp_check(model_gcc)
+pp_check(model_gcc, type = "dens_overlay_grouped", group = "Season")
 
 summary(model_gcc)
 
