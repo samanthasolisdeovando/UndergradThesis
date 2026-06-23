fc561e0f06f84c24f9330c7b1812fe12a05df853
samanthasolisdeovando <slsolisdeovando@gmail.com>
2026-06-23 16:15:22 -0700

    updated comments

--- script/CommunityCompPCA.R
+++ script/CommunityCompPCA.R
@@ -7,37 +7,25 @@ master_data <- read.csv(file.choose("MasterDataSheet_UGthesis_Feb232026")) #usin
 
 head(master_data)
 
-########### only 1 timepoint dataset ##########
+#### Community matrix from 1 timepoint ####
 library(dplyr)
 only_june_11 <- master_data %>% filter(measurement.week == "5")
-
 write.csv(only_june_11, file = "june11_2_data_rowIDadded.csv")
-
-#### add row ID ####
 only_june_11 <- only_june_11 %>% mutate(row_id = row_number())
-
-########## community matrix using ONLY funct grp ##########
 community_matrix <- only_june_11 %>%
   select(shrub.cover, graminoid.cover, bryophyte.cover, forb.cover)
 
-########### run PCA ##########
+#### PCA scores --> datasheet ####
 library(vegan)
 pca_model <- rda(community_matrix)
-
-########## extract site scores ##########
 pca_scores <- as.data.frame(scores(pca_model, display = "sites"))
-
 pca_scores$row_id <- only_june_11$row_id
-
-######### join PCA scores back to datasheet ##########
-June_11_PCA <- only_june_11 %>%
-  left_join(pca_scores, by = "row_id")
+June_11_PCA <- only_june_11 %>% left_join(pca_scores, by = "row_id")
 
 write.csv(June_11_PCA, file = "June_11_PCA_.csv")
 
-######### PCA score scatter plot ##########
+#### PCA score scatter plot ####
 library(ggplot2)
-
 ggplot(June_11_PCA, aes(x = PC1, y = PC2, color = Treatment)) +
   geom_point(size = 3, alpha = 0.8) +
   theme_classic() +
@@ -45,7 +33,7 @@ ggplot(June_11_PCA, aes(x = PC1, y = PC2, color = Treatment)) +
   y = "PC2",
   color = "Treatment")
 
-###### PCA funct group biplot ########
+#### PCA funct group biplot ####
 
 species_scores <- as.data.frame(scores(pca_model, display = "species"))
 species_scores$FunctionalGroup <- rownames(species_scores)
@@ -54,8 +42,7 @@ species_scores$FunctionalGroup <- dplyr::recode(
   "shrub.cover" = "Shrubs",
   "graminoid.cover" = "Graminoids",
   "bryophyte.cover" = "Bryophytes",
-  "forb.cover" = "Forbs"
-)
+  "forb.cover" = "Forbs")
 
 ggplot(June_11_PCA, aes(x = PC1, y = PC2, color = Treatment)) +
   geom_point(size = 3, alpha = 0.7) +
